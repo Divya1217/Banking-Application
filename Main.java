ec9f33c931eea2be73592324620f7350e2d31b33
Divya1217 <126904774+Divya1217@users.noreply.github.com>
2024-12-30 02:13:36 -0800

    Add files via upload
    Created banking application simply using Java and oops concept.

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	        Account ac = new Account("2222",0,"Divya","Divya@gmail.com","344356");
+
+	        ac.DepositMoney(50);
+	        ac.DepositMoney(150);
+
+	        ac.WithDrawMoney(100);
+	    
+	    }
+	}
+
+
